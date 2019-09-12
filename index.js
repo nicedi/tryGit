46193dfe7f75d3bc6b4da421cc7053480fdf5d71
nicedi <nicedi@gmail.com>
2019-09-12 20:04:47 +0800

    Change var to const in index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 app.set('port', 3000);
 
 var handlebars = require('express-handlebars').create({defaultLayout: 'main'});
